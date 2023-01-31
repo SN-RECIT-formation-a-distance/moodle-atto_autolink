b2bfeddd058a76cb99be4fbe1f6941e268bcea1f
Gustavo Bazzo <gcbazzo@gmail.com>
2023-01-31 11:52:37 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -24,12 +24,12 @@
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022100100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023013100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'atto_recitautolink';  // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v1.17.0-beta';
-$plugin->supported = [39, 400];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.  
+$plugin->release = 'v1.17.1-stable';
+$plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.  
 $plugin->dependencies = [
-	'filter_recitactivity' => 2022100100
+	'filter_recitactivity' => 2023013100
 ];
-$plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
+$plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
